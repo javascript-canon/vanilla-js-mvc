6ffd13c18a3e0143f17700f980be94912cfbf888
Kai Gittens <kai.gittens@gmail.com>
2016-12-13 14:30:34 -0500

    add new files, delete old files

--- README.md
+++
@@ -1,4 +0,0 @@
-JavaScript Canon
-================
-
-Details are coming...
---
+++ js-build/model.js
@@ -0,0 +1,16 @@
+/* ================================================================= */
+/* | MODEL DATA                                                      */
+/* ================================================================= */
+/*                                                                   */
+/*   -  A very simple MVC implementation-read more at:               */
+/*      http://bit.ly/1zxWh0m                                        */
+/* ================================================================= */
+
+// use strict mode
+"use strict";
+
+// Reference the model data on Heroku
+var ResourceModel = "/api/resources";
+
+// Export the model out so it can be required properly
+exports.ResourceModel = ResourceModel;
